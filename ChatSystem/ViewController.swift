6147b6ea1afee460d18f9c145e32cb62bbe0214d
Pham Van Quan <quan.pham.van@socialfintech.net>
2019-03-26 09:23:08 +0700

    Initial Commit

---
+++ ChatSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatSystem
+//
+//  Created by Pham Van Quan on 3/26/19.
+//  Copyright © 2019 Pham Van Quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
