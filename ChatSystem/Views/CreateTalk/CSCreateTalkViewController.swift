184804fbc92834ac77f7c9894a3e66abddb0228d
Pham Van Quan <quan.pham.van@socialfintech.net>
2019-03-28 16:12:46 +0700

    [QuanPV] mockup for member talk screen

--- ChatSystem/Views/CreateTalk/CSCreateTalkViewController.swift
+++ ChatSystem/Views/CreateTalk/CSCreateTalkViewController.swift
@@ -8,22 +8,44 @@
 
 import UIKit
 
-class CSCreateTalkViewController: CSBaseViewController {
+class CSCreateTalkViewController: CSBaseViewController , UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var rooms = [Room(name: "Red Velvet", date: "2019/01/11 11:30"),
+                 Room(name: "Brownie", date: "2019/02/13 12:30"),
+                 Room(name: "Bannna Bread", date: "2019/03/16 10:00"),
+                 Room(name: "Vanilla", date: "2019/04/14 01:30"),
+                 Room(name: "Minty", date: "2019/05/15 09:10")]
+    
+    var filteredRooms = [Room]()
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if (self.responds(to: #selector(getter: UIViewController.edgesForExtendedLayout))) {
-              self.edgesForExtendedLayout = []
+              self.edgesForExtendedLayout = UIRectEdge.init(rawValue: 0)
         }
-      
-        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width:  self.view.bounds.width, height: 45))
+        let heightStatusBar = UIApplication.shared.statusBarFrame.height
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: heightStatusBar, width:  self.view.bounds.width, height: 45))
         self.view.addSubview(navBar)
-        navBar.backgroundColor = UIColor.red
-        let navItem = UINavigationItem(title: "News");
+        navBar.backgroundColor = UIColor.cyan
+        let navItem = UINavigationItem(title: "トークルーム")
         let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: nil, action: #selector(actionBack));
         navItem.leftBarButtonItem = doneItem;
         navBar.setItems([navItem], animated: false);
         // Do any additional setup after loading the view.
+        filteredRooms = rooms
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        self.tableView.tableHeaderView = searchController.searchBar
+        
+        self.tableView.registerCellNib(CSMemberTalkCell.self)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
     @objc func actionBack() {
@@ -39,4 +61,42 @@ class CSCreateTalkViewController: CSBaseViewController {
     }
     */
 
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // If we haven't typed anything into the search bar then do not filter the results
+        if searchController.searchBar.text! == "" {
+            filteredRooms = rooms
+        } else {
+            // Filter the results
+            filteredRooms = rooms.filter { $0.name.lowercased().contains(searchController.searchBar.text!.lowercased()) }
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredRooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a new cell if needed or reuse an old one
+        var cell:CSMemberTalkCell? = self.tableView.dequeueReusableCell(withIdentifier: CSMemberTalkCell.className, for: indexPath) as? CSMemberTalkCell
+        if (cell == nil) {
+            cell = CSMemberTalkCell(style: .default, reuseIdentifier:
+                CSMemberTalkCell.className)
+        }
+        // set the text from the data model
+        cell?.setData(filteredRooms[indexPath.row])
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
 }
---
+++ ChatSystem/Views/CreateTalk/Cell/CSMemberTalkCell.swift
@@ -0,0 +1,34 @@
+//
+//  CSMemberTalkCell.swift
+//  ChatSystem
+//
+//  Created by Pham Van Quan on 3/28/19.
+//  Copyright © 2019 Pham Van Quan. All rights reserved.
+//
+
+import UIKit
+
+class CSMemberTalkCell: UITableViewCell {
+
+    @IBOutlet weak var rationButton: UIButton!
+    @IBOutlet weak var imageAvatar: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setData(_ room: Room) {
+      self.labelName.text = room.name
+    }
+    
+    @IBAction func actionSelected(_ sender: UIButton) {
+         self.rationButton.isSelected =  !sender.isSelected
+    }
+}
